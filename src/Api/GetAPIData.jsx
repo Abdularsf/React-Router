6bb140b8a29268373eb376fd644b06186b80ebdc
Abdul Rahman <abdularsf147@gmail.com>
2025-11-03 18:44:44 +0530

    ND

--- src/Api/GetAPIData.jsx
+++ src/Api/GetAPIData.jsx
@@ -1,6 +1,6 @@
 export const getMoviesData  = async() =>{
     try {
-        const response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=342e4c02&s=titanic&page1");
+        const response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=342e4c02&s=Avengers&page1");
         const data = await response.json();
         return data;
     } catch (error) {
